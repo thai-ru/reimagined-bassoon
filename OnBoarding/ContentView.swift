17453b93cf43419fe587cc86040dc5b89fec5179
thai-ru <robinthairu34@gmail.com>
2024-04-11 21:56:13 +0300

    Initial Commit

---
+++ OnBoarding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnBoarding
+//
+//  Created by Robinson Thairu on 11/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnBoarding/OnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingApp.swift
+//  OnBoarding
+//
+//  Created by Robinson Thairu on 11/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
