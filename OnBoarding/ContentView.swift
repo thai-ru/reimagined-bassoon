48cdd054e29088847081ceabaad52f06488c5d33
thai-ru <robinthairu34@gmail.com>
2024-04-11 23:05:45 +0300

    tab view: pages

--- OnBoarding/ContentView.swift
+++ OnBoarding/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            WelcomePage()
+            FeaturesPage()
         }
-        .padding()
+        .tabViewStyle(.page)
     }
 }
 
