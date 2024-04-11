4963f100d044193a85b34e15ce0f24c61e06dea9
thai-ru <robinthairu34@gmail.com>
2024-04-11 22:30:37 +0300

    welcome page

---
+++ OnBoarding/Welcomepage.swift
@@ -0,0 +1,18 @@
+//
+//  Welcomepag.swift
+//  OnBoarding
+//
+//  Created by Robinson Thairu on 11/04/2024.
+//
+
+import SwiftUI
+
+struct Welcomepag: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Welcomepag()
+}
---
+++ WelcomePage.swift
@@ -0,0 +1,44 @@
+//
+//  WelcomePage.swift
+//  OnBoarding
+//
+//  Created by Robinson Thairu on 11/04/2024.
+//
+
+import SwiftUI
+
+struct WelcomePage: View {
+    var body: some View {
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 30)
+                    .frame(width: 150, height: 150)
+                    .foregroundStyle(.tint)
+                
+                Image(systemName: "figure.2.and.child.holdinghands")
+                    .font(.system(size: 70))
+                    .foregroundStyle(.white)
+            }
+            
+            Text("Welcome to MyApp")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .fontWidth(.condensed)
+            .fontDesign(.serif)
+            .padding(.top)
+            .padding(.bottom)
+            
+            Text("Welcome, Let's get started!")
+                .font(.headline)
+                .fontWeight(.semibold)
+                .fontWidth(.expanded)
+                .fontDesign(.serif)
+        }
+        .padding()
+        .multilineTextAlignment(.center)
+    }
+}
+
+#Preview {
+    WelcomePage()
+}
